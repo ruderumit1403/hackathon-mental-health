446b8060759148870300f3016b9f1110c48ff1d7
rumit nagori <83534966+ruderumit1403@users.noreply.github.com>
2021-05-28 08:48:43 +0530

    Create url.py

---
+++ mental/url.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url
+from mental import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.index, name='index'),
+    url(r'^users', views.users, name='users'),
+    url(r'^voltages', views.voltages, name='voltages'),
+    url(r'^user/(?P<user_name_url>\w+)/$', views.user, name='user'),
+    url(r'^addvoltages', views.addvoltages),
+    url(r'^cleardb', views.cleardb),
+    url(r'^keylog', views.keylog),
+    )
